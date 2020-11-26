3ad241049066164b08f994b1bf0589e3f1e99158
Guoli Lyu <guoli-lv@hotmail.com>
2020-11-26 09:41:42 +0800

    fix: check item mod content before unwrap

--- src/visit/attr.rs
+++ src/visit/attr.rs
@@ -25,6 +25,7 @@ pub fn find_attribute(attrs: &Vec<Attribute>, to_match: &str) -> bool {
         < attrs.len()
 }
 
+#[allow(dead_code)]
 pub fn pop_attribute(attrs: &mut Vec<Attribute>, ident: &str) -> Option<String> {
     let mut result = None;
     attrs.retain(|attr| {
@@ -71,74 +72,78 @@ pub fn remove_matched_attribute(
 }
 
 pub fn mod_remove_items(item_mod: &mut ItemMod, remove: &str) {
-    // remove item that has attribute of remove
-    item_mod.content.as_mut().unwrap().1.retain(|item| {
-        let found = match item {
-            Item::Impl(item) => attr::find_attribute(&item.attrs, remove),
-            Item::Trait(item) => attr::find_attribute(&item.attrs, remove),
-            Item::Struct(item) => attr::find_attribute(&item.attrs, remove),
-            Item::Enum(item) => attr::find_attribute(&item.attrs, remove),
-            Item::Fn(item) => attr::find_attribute(&item.attrs, remove),
-            Item::Mod(item) => attr::find_attribute(&item.attrs, remove),
-            _ => false,
-        };
-        !found
-    });
-    // recursively remove item under mod
-    item_mod
-        .content
-        .as_mut()
-        .unwrap()
-        .1
-        .iter_mut()
-        .for_each(|item| {
-            if let Item::Mod(child_item_mod) = item {
-                mod_remove_items(child_item_mod, remove)
-            }
-        })
+    if item_mod.content.is_some() {
+        // remove item that has attribute of remove
+        item_mod.content.as_mut().unwrap().1.retain(|item| {
+            let found = match item {
+                Item::Impl(item) => attr::find_attribute(&item.attrs, remove),
+                Item::Trait(item) => attr::find_attribute(&item.attrs, remove),
+                Item::Struct(item) => attr::find_attribute(&item.attrs, remove),
+                Item::Enum(item) => attr::find_attribute(&item.attrs, remove),
+                Item::Fn(item) => attr::find_attribute(&item.attrs, remove),
+                Item::Mod(item) => attr::find_attribute(&item.attrs, remove),
+                _ => false,
+            };
+            !found
+        });
+        // recursively remove item under mod
+        item_mod
+            .content
+            .as_mut()
+            .unwrap()
+            .1
+            .iter_mut()
+            .for_each(|item| {
+                if let Item::Mod(child_item_mod) = item {
+                    mod_remove_items(child_item_mod, remove)
+                }
+            })
+    }
 }
 
 pub fn mod_remove_attr(item_mod: &mut ItemMod, preserve: &str) {
-    // remove item that has attribute of remove
-    item_mod
-        .content
-        .as_mut()
-        .unwrap()
-        .1
-        .iter_mut()
-        .for_each(|item| {
-            match item {
-                Item::Impl(item) => {
-                    attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+    // remove attr that has attribute of preserve
+    if item_mod.content.is_some() {
+        item_mod
+            .content
+            .as_mut()
+            .unwrap()
+            .1
+            .iter_mut()
+            .for_each(|item| {
+                match item {
+                    Item::Impl(item) => {
+                        attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+                    }
+                    Item::Trait(item) => {
+                        attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+                    }
+                    Item::Struct(item) => {
+                        attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+                    }
+                    Item::Enum(item) => {
+                        attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+                    }
+                    Item::Fn(item) => {
+                        attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+                    }
+                    Item::Mod(item) => {
+                        attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
+                    }
+                    _ => (),
+                };
+            });
+        // recursively remove attributes of items under mod
+        item_mod
+            .content
+            .as_mut()
+            .unwrap()
+            .1
+            .iter_mut()
+            .for_each(|item| {
+                if let Item::Mod(child_item_mod) = item {
+                    mod_remove_attr(child_item_mod, preserve)
                 }
-                Item::Trait(item) => {
-                    attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
-                }
-                Item::Struct(item) => {
-                    attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
-                }
-                Item::Enum(item) => {
-                    attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
-                }
-                Item::Fn(item) => {
-                    attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
-                }
-                Item::Mod(item) => {
-                    attr::remove_matched_attribute(&mut item.attrs, "amphi", preserve);
-                }
-                _ => (),
-            };
-        });
-    // recursively remove attributes of items under mod
-    item_mod
-        .content
-        .as_mut()
-        .unwrap()
-        .1
-        .iter_mut()
-        .for_each(|item| {
-            if let Item::Mod(child_item_mod) = item {
-                mod_remove_attr(child_item_mod, preserve)
-            }
-        })
+            })
+    }
 }
